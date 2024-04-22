772d694adfa360590014ed359a3ad289726887d6
Librado-Aguirre <122108830+Librado-Aguirre@users.noreplay.github.com>
2024-04-21 20:55:18 -0400

    Test commit for the RPS-UI branch

---
+++ RPS.js
@@ -0,0 +1,8 @@
+
+//let userSelection=prompt("Rock,Paper,Scissor");
+function getComputerChoice (){
+    let userSelection = prompt("Rock, Paper, Scissor")
+    document.getElementsById("demo").innerHTML = computerSelection
+    let computerSelection=prompt("Rock,Paper,Scissor")
+
+}
